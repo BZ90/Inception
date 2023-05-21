3e8eb1bd3e75942d0328a332510f7d11c96aba39
BZ <95119250+BZ90@users.noreply.github.com>
2023-05-21 16:38:21 +0930

    Added everything except .env

---
+++ Makefile
@@ -0,0 +1,32 @@
+RED = \033[1;31m
+GREEN = \033[1;32m
+YELLOW = \033[1;33m
+PLAIN = \033[0m
+
+all: up
+
+down:
+	docker compose -f ./srcs/docker-compose.yml down
+
+up:
+	@echo "$(GREEN)Building and Starting Containers$(PLAIN)"
+	docker compose -f ./srcs/docker-compose.yml up --build
+
+start:
+	@echo "$(GREEN)Starting Stopped Containers$(PLAIN)"
+	docker compose -f ./srcs/docker-compose.yml start
+
+stop:
+	@echo "$(RED)Stopping Containers$(PLAIN)"
+	docker compose -f ./srcs/docker-compose.yml stop
+
+prune:
+	@echo "$(RED)Removing all unused stuff$(PLAIN)"
+	docker system prune -f
+
+clean: down prune
+	@echo "$(RED)Removing volumes$(PLAIN)"
+	@docker volume rm $$(docker volume ls -q)
+	@rm -rf /home/ben/data/mysql/* /home/ben/data/wordpress/*
+
+.PHONY: all down up clean
---
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+automate_secure_installation() {
+    /usr/bin/expect <<EOF
+        set timeout -1
+        spawn mysql_secure_installation
+        
+        expect "Enter current password for root (enter for none):"
+        send "\r"
+        
+        expect "Set root password?"
+        send "y\r"
+        
+        expect "New password:"
+        send "$MYSQL_ROOT_PASSWORD\r"
+        
+        expect "Re-enter new password:"
+        send "$MYSQL_ROOT_PASSWORD\r"
+        
+        expect "Remove anonymous users?"
+        send "y\r"
+        
+        expect "Disallow root login remotely?"
+        send "n\r"
+        
+        expect "Remove test database and access to it?"
+        send "y\r"
+        
+        expect "Reload privilege tables now?"
+        send "y\r"
+        
+        expect eof
+EOF
+}
+
+mysql_install_db
+
+/etc/init.d/mysql start
+
+if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]
+then
+	echo "Database already exists"
+
+else
+
+unset PROMPT
+
+automate_secure_installation
+
+echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -u root -p$MYSQL_ROOT_PASSWORD
+
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -u root -p$MYSQL_ROOT_PASSWORD
+
+mysql -u root -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /usr/local/bin/wordpress.sql
+
+fi
+
+/etc/init.d/mysql stop
+kill 176
+
+exec "$@"
